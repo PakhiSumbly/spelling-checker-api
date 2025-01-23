30d1d24bdaa43586d90e36f1f5110f04c668acf2
Pakhi <46786087+PakhiSumbly@users.noreply.github.com>
2025-01-23 15:41:22 +0530

    Update clients.py

--- clients.py
+++ clients.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://127.0.0.1:5002/correct'
 
-data = {'text': "It's bwwn so long"}
+data = {'text': "jam is coolder nowp"}                 #"she is wayyy too tally"  #"It's bwwn so long"   "birds flyy high"  #"jam is coolder nowp"
 
 response = requests.post(url, json=data)
 
